867db958eb111e96909833f17ab800a0f5246bb5
Zhanna Popenko <zpopenko7@gmail.com>
2021-11-11 15:01:24 +0200

    Add smooth scrolling animation

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
+
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      500,
+      "linear"
+    );
+  });
+});
